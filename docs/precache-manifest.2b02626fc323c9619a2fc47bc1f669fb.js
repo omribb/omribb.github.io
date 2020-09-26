af4cf6be7837944fed733679f064a25127608eba
Omri Ben Barak <omrib@alice.co.il>
2020-09-26 19:17:18 +0300

    added docs directory

---
+++ docs/precache-manifest.2b02626fc323c9619a2fc47bc1f669fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc50d1d06eefc2b3dff6ede1aa29de31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b5705675f8484e7f5dc",
+    "url": "/static/js/2.fbe812f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fbe812f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50be9a8b880b064e80f4",
+    "url": "/static/js/main.f2e8d2b3.chunk.js"
+  },
+  {
+    "revision": "d2116b1655bf966dfb7e",
+    "url": "/static/js/runtime-main.16cadf2e.js"
+  }
+]);
